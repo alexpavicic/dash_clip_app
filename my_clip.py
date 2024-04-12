51d15539fc1570971e2e21b347567a5546c4b693
Alex Pavicic <alexpavicicic@gmail.com>
2024-04-11 19:59:42 -0500

    Iterate over class airplane

--- my_clip.py
+++ my_clip.py
@@ -42,25 +42,26 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load('ViT-B/32', device)
 
 # Prepare the inputs
-image_path = image_list[3]
-image = Image.open(image_path)
+for i in range(10):  # Loop for the first 10 elements
+    image_path = image_list[i]
+    image = Image.open(image_path)
 
-image_input = preprocess(image).unsqueeze(0).to(device)
-text_inputs = torch.cat([clip.tokenize(f"a photo of a {c}") for c in class_folders]).to(device)
+    image_input = preprocess(image).unsqueeze(0).to(device)
+    text_inputs = torch.cat([clip.tokenize(f"a photo of a {c}") for c in class_folders]).to(device)
 
 # Calculate features
-with torch.no_grad():
-    image_features = model.encode_image(image_input)
-    text_features = model.encode_text(text_inputs)
+    with torch.no_grad():
+        image_features = model.encode_image(image_input)
+        text_features = model.encode_text(text_inputs)
 
 # Pick the top 5 most similar labels for the image
-image_features /= image_features.norm(dim=-1, keepdim=True)
-text_features /= text_features.norm(dim=-1, keepdim=True)
-similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
-values, indices = similarity[0].topk(5)
+    image_features /= image_features.norm(dim=-1, keepdim=True)
+    text_features /= text_features.norm(dim=-1, keepdim=True)
+    similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+    values, indices = similarity[0].topk(5)
 
 # Print the result
-print("\nTop predictions:\n")
-for value, index in zip(values, indices):
-    print(f"{class_folders[index]:>16s}: {100 * value.item():.2f}%")
+    print("\nTop predictions:\n")
+    for value, index in zip(values, indices):
+        print(f"{class_folders[index]:>16s}: {100 * value.item():.2f}%")
 
